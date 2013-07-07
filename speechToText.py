acf02d962fa467dae616f8c1a29cdeb772ceb3f9
Faizan <Slash1235@gmail.com>
2013-07-07 00:27:55 -0400

    updating speechToText

--- speechToText.py
+++ speechToText.py
@@ -1,8 +1,26 @@
 import urllib2
-url = "https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-US"
-audio = open('test.flac','rb').read()
-headers={'Content-Type': 'audio/x-flac; rate=16000', 'User-Agent':'Mozilla/5.0'}
-request = urllib2.Request(url, data=audio, headers=headers)
-response = urllib2.urlopen(request)
-print response.read()
-   
+
+def enter_command(file_name):
+	'''Using the file name provided, use the google speech api to have it return text from the audio file
+	File must be in flac format '''
+
+	url = "https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-US"
+	audio = open(file_name,'rb').read()
+	headers={'Content-Type': 'audio/x-flac; rate=16000', 'User-Agent':'Mozilla/5.0'}
+	request = urllib2.Request(url, data=audio, headers=headers)
+	response = urllib2.urlopen(request) #Grab response
+	return parse_response(eval(response.read())) #Return the response ( will need to parse after)
+
+def parse_response(response):
+	''' Parse the response given back by the google api, returns the text and the confidence level '''
+	#Example response: {"status":0,"id":"09cfbeabd8d01ab2d12b1af869c2e42f-1","hypotheses":[{"utterance":"play welcome to the jungle","confidence":0.9095006}]}
+	
+	resp = response['hypotheses'][0]['utterance']
+	confidence = response['hypotheses'][0]['confidence']
+	return [resp, confidence]
+
+
+
+
+if __name__ == "__main__":
+	print enter_command('test.flac')   
